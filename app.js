67b471972ecdbd5766470cb002615aac6972ed2e
Md Akmal <amdryzen333@gmail.com>
2026-07-08 08:58:02 +0530

    added stylings in all listing ejs pages

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
+const engine = require('ejs-mate');
+app.use(express.static(path.join(__dirname,"public")));
 
-
+app.engine('ejs', engine);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+
 
 
 main().catch(err => console.log(err)).then(()=>{console.log("Connected to db")});
@@ -63,7 +67,7 @@ app.get("/listings/:id",async(req,res)=>{
 //create route
 app.post("/listings",async(req,res)=>{
     console.log(req.body.listing);
-    const newListing= await new Listing(req.body.listing).save();
+    const newListing= await new Listing(req.body.listing).save();//dont forget .save
     res.redirect("/listings");
 });
 
