5a830715584c55c5c781cf46f03473b8602ab243
Md Akmal <amdryzen333@gmail.com>
2026-08-03 22:30:26 +0530

    Restructuring code usinf Router.route & added stars using starability library

--- routes/listing.js
+++ routes/listing.js
@@ -8,26 +8,33 @@ const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 
 const listingController=require("../controller/listing.js");
 
-//index route
-router.get("/",wrapAsync(listingController.index));
-
-//new route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
-//show route
-router.get("/:id",wrapAsync(listingController.showListing));
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing));
 
-//create route
-router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
 
-//edit route
 router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
 
-//update route
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
 
-//delete route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports=router;
